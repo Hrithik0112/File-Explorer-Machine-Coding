de580d64b636fda872d6954c1d18809890bf01c5
Hrithik <slumdog4321@gmail.com>
2024-09-09 22:41:34 +0530

    Advance : Collapsable File folder tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { ChevronRightIcon } from "@heroicons/react/16/solid";
 import { DocumentIcon, FolderIcon } from "@heroicons/react/24/solid";
+import { useState } from "react";
 
 type Folder = {
   name: string;
@@ -55,21 +57,32 @@ function App() {
 }
 
 const Folder = ({ folder }: { folder: Folder }) => {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <li className="my-1.5" key={folder.name}>
       <span className="flex items-center gap-1.5">
+        {folder.folders && folder.folders.length > 0 && (
+
+        <button onClick={()=> setIsOpen(!isOpen)}>
+
+        <ChevronRightIcon className={`size-4 text-gray-500 ${isOpen ? "rotate-90" : ""} `}/>
+        </button>
+        )}
         {folder.folders ? (
-          <FolderIcon className="size-6 text-sky-500" />
+          <FolderIcon className={`size-6 text-sky-500 ${folder.folders.length === 0 ? "ml-[22px]" : ""}`} />
         ) : (
-          <DocumentIcon className="size-6 text-gay-900" />
+          <DocumentIcon className=" ml-[22px] size-6 text-gay-900" />
         )}
         {folder.name}
       </span>
+      {isOpen && (
+
       <ul className="pl-6">
         {folder.folders?.map((folder) => (
           <Folder folder={folder} key={folder.name} />
         ))}
       </ul>
+      )}
     </li>
   );
 };
